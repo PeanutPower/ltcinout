388f27adb6931dd8aba41aca5d78ffc38ca0f3d7
PeanutPower <support@ltcgaming.tk>
2012-09-21 19:43:56 +0100

    move config into separate file and provide template for install

---
+++ config-template.php
@@ -0,0 +1,11 @@
+<?php
+
+// rename this file to config.php and fill in the values below
+
+$db_host = ""; 				// mysql host
+$db_user = ""; 				// mysql username
+$db_pass = ""; 				// mysql password
+$db_name = ""; 				// mysql database name
+$db_port = 3306;
+
+?>
